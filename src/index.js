78598f1dfaf5e7277e4630cd8e79576f2c15f033
João Ferraz (@p1padev) <jvkferraz@gmail.com>
2024-05-26 20:49:56 -0300

    Starting UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-// ES6 code snippet
-export const greet = (name) => `Hello, ${name}! Welcome to ES6.`;
-
-const person = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 30,
-};
-
-const { firstName, lastName, age } = person;
+console.log('Starting UI');
